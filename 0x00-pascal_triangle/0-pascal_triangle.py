66db4f4962f79f74803e7645e90a9d28f3fd9969
Ousman <usman.traid@gmail.com>
2023-04-27 10:15:35 -0400

    code refactored

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -9,44 +9,18 @@ def pascal_triangle(n):
         return []
 
     """ initialize an empty resulting array """
-    pascal = [[] for idx in range(n)]
+    triangle = [[] for idx in range(n)]
 
-    for li in range(n):
-        for col in range(li+1):
-            if(col < li):
+    for row in range(n):
+        for col in range(row + 1):
+            if(col < row):
                 if(col == 0):
                     """ the first column is always set to 1 """
-                    pascal[li].append(1)
+                    triangle[row].append(1)
                 else:
-                    pascal[li].append(pascal[li-1][col] + pascal[li-1][col-1])
-            elif(col == li):
+                    triangle[row].append(triangle[row-1][col - 1] + triangle[row-1][col])
+            elif(col == row):
                 """ the diagonal is always set to 1 """
-                pascal[li].append(1)
+                triangle[row].append(1)
 
-    return pascal
-
-# #!/usr/bin/python3
-# """
-# Pascal Triangle
-# """
-
-
-# def pascal_triangle(n):
-#     """Returns a list of lists of intergers representing
-#     Pascal's triangle"""
-#     if n <= 0:
-#         return []
-#     if n == 1:
-#         return [1]
-#     triangle = [pascal_triangle(1)]
-#     for i in range(n - 1):
-#         row = [1]
-#         prev_row = triangle[len(triangle) - 1]
-#         for i in range(len(prev_row)):
-#             if i <= len(prev_row) - 2:
-#                 row.append(prev_row[i] + prev_row[i + 1])
-#             else:
-#                 row.append(1)
-#         triangle.append(row)
-
-#     return triangle
+    return triangle
