63d957063b06a7916fa8c50acbc8abb665816de6
Ousman <usman.traid@gmail.com>
2023-05-04 18:30:18 -0400

    task completed

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -12,26 +12,88 @@ def minOperations(n):
     operations needed to result in exactly n H characters in the file.
     """
 
-    operations = 0
-    if n <= 1 or type(n) is not int:
-        return 0
+    # operations = 0
+    # if n <= 1:
+    #     return 0
 
-    for i in range(2, n + 1):
+    # for i in range(2, n + 1):
         """
         check if i is a factor of n
         """
-        while n % i == 0:
+        # while n % i == 0:
 
             """
             if i is a factor of n, break n into smaller
             parts to find the highest possible factor of
             n which i it's multiple
             """
-            n = n / i
+            # n = n / i
 
             """
             then sum up all the number of smaller parts
             (paste smaller parts number of factor 'i' times)
             """
-            operations += i
-    return operations
+            # operations += i
+    # return operations
+
+
+#!/usr/bin/python3
+"""Minimum Operations Module"""
+
+
+def minOperations(n):
+    """
+    Calculates the fewest number of operations needed to
+    result in exactly n `H` characters in the file. The
+    only operations allowed are `Copy All` and `Paste`.
+
+    Returns an integer.
+    If n is impossible to achieve, return 0.
+
+    Example:
+
+    n = 9
+
+    H => Copy All => Paste => HH => Paste => HHH =>
+    Copy All => Paste => HHHHHH => Paste => HHHHHHHHH
+
+    Number of operations: 6
+    """
+    total_chars = 1  # no. of H's in the file
+    clipboard = 0  # no. of H's copied
+    counter = 0  # operations counter
+
+    while total_chars < n:
+        # if nothing has been copied yet
+        if clipboard == 0:
+            # copy all
+            clipboard = total_chars
+            # paste
+            total_chars += clipboard
+            # increment operations counter
+            counter += 2
+            # continue to next loop
+            continue
+
+        remaining = n - total_chars  # remaining chars to paste
+        # if clipboard contains more chars than remaining
+        # it's impossible to arrive at the desired result
+        if remaining < clipboard:
+            return 0
+
+        # if remaining is not a multiple of the total_chars
+        # we only paste what's in the clipboard
+        if remaining % total_chars != 0:
+            # paste current clipboard
+            total_chars += clipboard
+            # increment operations counter
+            counter += 1
+        else:
+            # copy all
+            clipboard = total_chars
+            # paste
+            total_chars += clipboard
+            # increment operations counter
+            counter += 2
+
+    return counter
