c98c410c8a385797125cdb04c1c79922b041f74f
Ousman <usman.traid@gmail.com>
2023-05-04 18:31:58 -0400

    task completed

---
+++ 0x02-minimum_operations/tt.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+0-minoperations
+"""
+
+
+def minOperations(n):
+    """
+    In a text file, there is a single character H. Your text editor
+    can executeonly two operations in this file: Copy All and Paste.
+    Given a number n, this a method calculates the fewest number of
+    operations needed to result in exactly n H characters in the file.
+    """
+
+    operations = 0
+    if n <= 1:
+        return 0
+
+    for i in range(2, n + 1):
+        """
+        check if i is a factor of n
+        """
+        while n % i == 0:
+
+            """
+            if i is a factor of n, break n into smaller
+            parts to find the highest possible factor of
+            n which i it's multiple
+            """
+            n = n / i
+
+            """
+            then sum up all the number of smaller parts
+            (paste smaller parts number of factor 'i' times)
+            """
+            operations += i
+    return operations
