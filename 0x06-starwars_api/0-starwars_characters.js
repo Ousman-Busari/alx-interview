4c9eaf814f109025929251306feb191d923b1da1
Ousman <usman.traid@gmail.com>
2023-06-01 17:17:20 -0400

    promise used to get charcaters in order

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,19 +1,22 @@
 #!/usr/bin/node
-const request = require('request');
+const request = require("request");
 
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/';
-request(url, function (error, response, body) {
+const url = "https://swapi-api.hbtn.io/api/films/" + process.argv[2] + "/";
+request(url, async (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
     const characters = JSON.parse(body).characters;
     for (const character of characters) {
-      request(character, function (error, response, body) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
+      await new Promise((resolve, reject) => {
+        request(character, function (error, response, body) {
+          if (error) {
+            reject(error);
+          } else {
+            console.log(JSON.parse(body).name);
+            resolve();
+          }
+        });
       });
     }
   }
