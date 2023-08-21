3aca4c6a90f5d3bb8f2c3f6c2d3fccb602708836
Ousman <usman.traid@gmail.com>
2023-08-21 17:05:42 -0400

    all tasks completed

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Change comes from within"""
+
+
+def makeChange(coins, total):
+    """ Determines the fewest number of coins
+    needed to meet a given amount total
+    """
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    numCoins = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        numCoins += total // coin
+        total = total % coin
+    if total != 0:
+        return -1
+    return numCoins
